082b10fc42925030ecb6cf2d9b51d5cb58f53e22
lijundong <leeonfield@foxmail.com>
2019-07-05 17:32:08 +0800

    feat: 升级 gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,28 @@
-const fs = require('fs-extra');
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-const watch = require('gulp-watch');
+const del = require('del');
 
-gulp.task('default', ['clearLib', 'compileJS', 'moveConfig']);
-
-gulp.task('clearLib', [], function() {
-    return fs.removeSync('./lib/')
+gulp.task('clearLib', function() {
+    return del(['lib']);
 });
 
-gulp.task('compileJS', [], function() {
-	var babelProcess = babel({
+gulp.task('compileJS', function() {
+    var babelProcess = babel({
         presets: ['es2015', 'es2017'],
         plugins: ['transform-runtime']
-    })
-
-    babelProcess.on('error', function(e) {
+    }).on('error', function(e) {
         console.log(e);
         process.exit(1);
     });
 
-    return watch('src/**/*.js', {
+    return gulp.src('src/**/*.js', {
         verbose: true,
         ignoreInitial: false
     }).pipe(babelProcess).pipe(gulp.dest('lib'));
 })
 
-gulp.task('moveConfig', [], function() {
-    return watch(['src/config/**/*.*', 'src/config/**/.*'], {
-        verbose: true,
-        ignoreInitial: false
-    }, () => {
-        gulp.src(['src/config/**/*.*', 'src/config/**/.*'], {base: 'src/'}).pipe(gulp.dest('lib'));
-    })
+gulp.task('moveConfig', function() {
+    return gulp.src(['src/config/**/*.*', 'src/config/**/.*'], {base: 'src/'}).pipe(gulp.dest('lib'));
 });
+
+gulp.task('default', gulp.series('clearLib', gulp.parallel('compileJS', 'moveConfig')));
